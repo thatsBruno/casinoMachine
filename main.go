84e2149067c84047606dacc577629d01802dd65d
Bruno <brunoandreribeiro18@gmail.com>
2024-11-09 15:06:46 +0000

    add getName

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+func getName() {
+	name := ""
+
+	fmt.Println("Welcome to the Casino...")
+	fmt.Printf("Enter your name: ")
+	fmt.Scanln(&name)
+}
+
 func main() {
 
 }
